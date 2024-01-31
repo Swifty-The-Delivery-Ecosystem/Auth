fcb79f36b56f79902f9777d0def4efd2ea331203
Aditya062003 <adityavinay@iitbhilai.ac.in>
2024-01-31 14:18:05 +0530

    Added get vendors api

--- controllers/adminAuth.controller.js
+++ controllers/adminAuth.controller.js
@@ -150,3 +150,19 @@ exports.getAllUsers = async (req, res, next) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+exports.getAllVendors = async (req, res, next) => {
+  try {
+    const vendors = await Vendor.find({});
+    if (!vendors) {
+      return res.status(404).json({ error: "users not found" });
+    }
+
+    res.status(200).json({
+      vendors: vendors,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- routes/adminAuth.route.js
+++ routes/adminAuth.route.js
@@ -3,14 +3,26 @@ const router = express.Router();
 
 const checkAdmin = require("../middlewares/checkAdmin");
 const checkAuth = require("../middlewares/checkAuth");
-const {adminLogin, vendorApplicationStatusView, changeVendorApplicationStatus, debarVendor, debarUser, getAllUsers} = require("../controllers/adminAuth.controller")
-
+const {
+  adminLogin,
+  vendorApplicationStatusView,
+  changeVendorApplicationStatus,
+  debarVendor,
+  debarUser,
+  getAllUsers,
+  getAllVendors,
+} = require("../controllers/adminAuth.controller");
 
 router.post("/login", adminLogin);
-router.get("/new_vendors",  checkAdmin,vendorApplicationStatusView);
-router.put("/vendors/:vendorId/status", checkAdmin,changeVendorApplicationStatus );
-router.put("/vendor", checkAdmin,debarVendor );
-router.put("/user", checkAdmin,debarUser );
-router.get("/users/view", checkAdmin,getAllUsers );
+router.get("/new_vendors", checkAdmin, vendorApplicationStatusView);
+router.put(
+  "/vendors/:vendorId/status",
+  checkAdmin,
+  changeVendorApplicationStatus
+);
+router.put("/vendor", checkAdmin, debarVendor);
+router.put("/user", checkAdmin, debarUser);
+router.get("/users/view", checkAdmin, getAllUsers);
+router.get("/vendors/view", checkAdmin, getAllVendors);
 
 module.exports = router;
