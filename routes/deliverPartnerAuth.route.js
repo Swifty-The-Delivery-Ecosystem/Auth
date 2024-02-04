85c9902be2e71aae43e6c8bb3a8399b13ee974c5
Aditya062003 <adityavinay@iitbhilai.ac.in>
2024-02-05 03:02:36 +0530

    Added delivery partner api

--- routes/deliverPartnerAuth.route.js
+++ routes/deliverPartnerAuth.route.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const checkAuth = require("../middlewares/checkDeliveryPartner");
-const {deliveryPartnerLogin, updateAvailability} = require("../controllers/deliveryPartner.controller");
+const {
+  deliveryPartnerLogin,
+  updateAvailability,
+  getDeliveryPartner,
+} = require("../controllers/deliveryPartner.controller");
 
-router.post("/login", deliveryPartnerLogin );
+router.post("/login", deliveryPartnerLogin);
 
-router.put("/availability_status",checkAuth, updateAvailability);
+router.put("/availability_status", checkAuth, updateAvailability);
+router.get("/get_delivery_boy/:delivery_boy_id", getDeliveryPartner);
 
 module.exports = router;
